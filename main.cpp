8c5d60595b743f505319068dc01a6113692da0bb
Yaroslav <yaroslav.bojko.82@bk.ru>
2024-01-11 10:56:02 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int a,b,c;
+    std::cout << "Enter the parameters of the first box (A,B,C): ";
+    std::cin >> a >> b >> c;
+
+    int m,n,k;
+    std::cout << "Enter the parameters of the second box (m,n,k): ";
+    std::cin >> m >> n >> k;
+
+    if ((m > a && n > b && k > c) || (m > a && n > c && k > b) || (m > b && n > a && k > c) ||
+        (m > b && n > c && k > a) || (m > c && n > a && k > b) || (m > c && n > b && k > a))
+        std::cout << "The first box CAN be placed inside the second.";
+    else
+        std::cout << "The first box CANNOT be placed inside the second.";
 }
